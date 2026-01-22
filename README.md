aabe43e2750c5b74d8a0a6f786d4d437d31c0700
Vishal Wagh <vishalrwagh1999@gmail.com>
2026-01-22 08:38:43 +0530

    my first commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
+<<<<<<< HEAD
 # git-and-github-Practicals
 executing from local to GitHub
+=======
+# My git workspace commands
+>>>>>>> 1ca947b (my first commit)
---
+++ hello_world.java
@@ -0,0 +1,5 @@
+public class hello_world {
+    public static void main(String[] args) {
+        System.out.println("Welcome to DevOps day 2 session");
+    }
+}
